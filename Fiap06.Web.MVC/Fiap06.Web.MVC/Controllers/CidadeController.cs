473e11bbd503f2b57cd58dd1e48694d5e811d02a
Logon PF Fiap <logonpflocal@fiap.com.br>
2018-09-17 11:47:24 -0300

    Repository e Unit Of Work

---
+++ Fiap06.Web.MVC/Fiap06.Web.MVC/Controllers/CidadeController.cs
@@ -0,0 +1,55 @@
+﻿using Fiap06.Web.MVC.Models;
+using Fiap06.Web.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap06.Web.MVC.Controllers
+{
+    public class CidadeController : Controller
+    {
+        private PaisContext _context = new PaisContext();
+
+        [HttpGet]
+        public ActionResult Buscar(int? estado)
+        {
+            CarregarCombo();            
+            var lista = _context.Cidades.Include("Estado")
+                    .Where(c => c.EstadoId == estado || 
+                     estado == null).ToList();
+            return View("Listar",lista);
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            CarregarCombo();
+            var lista = _context.Cidades.Include("Estado").ToList();
+            return View(lista);
+        }
+
+        private void CarregarCombo()
+        {
+            var ufs = _context.Estados.ToList();
+            ViewBag.estados = new SelectList(ufs, "EstadoId", "Nome");
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            CarregarCombo();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Cidade cidade)
+        {
+            _context.Cidades.Add(cidade);
+            _context.SaveChanges();
+            TempData["msg"] = "Cidade cadastrada";
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
---
+++ Fiap06.Web.MVC/Fiap06.Web.MVC/UnitsOfWork/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using Fiap06.Web.MVC.Persistencia;
+using Fiap06.Web.MVC.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap06.Web.MVC.UnitsOfWork
+{
+    public class UnitOfWork : IDisposable
+    {
+        private PaisContext _context = new PaisContext();
+
+        private IEstadoRepository _estadoRepository;
+
+        public IEstadoRepository EstadoRepository
+        {
+            get
+            {
+                if (_estadoRepository == null)
+                {
+                    _estadoRepository = new EstadoRepository(_context);
+                }
+                return _estadoRepository;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+            GC.SuppressFinalize(this);
+        }
+
+        public void Salvar()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
